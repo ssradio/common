7c139ed0f4a4b93d96c3bd3f4fedc841e9bdc002
Matt Sims <matt@deucecreative.co.uk>
2017-05-04 19:08:12 +0100

    Check JSON data is valid.

--- src/tracklist.php
+++ src/tracklist.php
@@ -7,10 +7,18 @@ class Tracklist {
 	public function __construct() {
 	}
 
-	public static function from_arp_json() {
+	public static function from_arp_json( $json ) {
+
 		$tracklist = new Tracklist;
 
-		// TODO: Process JSON
+		$arp_data = json_decode( $json );
+
+		if ( is_object( $arp_data ) && isset( $arp_data->results ) && ! empty( $arp_data->results ) ) {
+
+			foreach ( $arp_data->results as $result ) {
+				// TODO: Process ARP results
+			}
+		}
 
 		return $tracklist;
 	}
