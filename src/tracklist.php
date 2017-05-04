c6b3b511e58df3f29c52c72da8a1c2ad237c7b1e
Matt Sims <matt@deucecreative.co.uk>
2017-05-04 10:20:02 +0100

    Add skeleton Tracklist class.

---
+++ src/tracklist.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SSRadio\Common;
+
+class Tracklist {
+
+	public function __construct() {
+	}
+
+	public static function from_arp_json() {
+		$tracklist = new Tracklist;
+
+		// TODO: Process JSON
+
+		return $tracklist;
+	}
+}
