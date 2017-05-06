7ed17f9af6544c677eb6d7a65e9a2e8a9043053a
Matt Sims <matt@deucecreative.co.uk>
2017-05-06 13:39:21 +0100

    Tracklist development.

--- src/tracklist.php
+++ src/tracklist.php
@@ -4,22 +4,38 @@ namespace SSRadio\Common;
 
 class Tracklist {
 
+	protected $tracklist;
+
 	public function __construct() {
 	}
 
 	public static function from_arp_json( $json ) {
 
-		$tracklist = new Tracklist;
+		$instance = new Tracklist;
 
 		$arp_data = json_decode( $json );
 
 		if ( is_object( $arp_data ) && isset( $arp_data->results ) && ! empty( $arp_data->results ) ) {
 
-			foreach ( $arp_data->results as $result ) {
+			$results = $arp_data->results;
+
+			foreach ( $results->result as $result ) {
+
+				error_log( print_r( $result, true ) );
+
 				// TODO: Process ARP results
+				$track = [
+					'start' => intval( $result->start ),
+					'end' => intval( $result->end ),
+				];
+				$instance->tracklist[] = $track;
 			}
 		}
 
-		return $tracklist;
+		return $instance;
+	}
+
+	public function __toString() {
+		return print_r( $this->tracklist, true );
 	}
 }
